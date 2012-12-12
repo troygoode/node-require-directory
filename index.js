78d9dba397744a9031e6094bdb2d1fbcdacfbb85
Troy Goode <troygoode@gmail.com>
2012-12-12 10:57:06 -0500

    can now pass a delegate into require-directory

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ var fs = require('fs');
 var join = require('path').join;
 var resolve = require('path').resolve;
 
-var requireDirectory = function(m, path){
+var requireDirectory = function(m, path, delegate){
+  if(delegate === null || delegate === undefined) {
+    delegate = function(){ return true; };
+  }
   var retval = {};
   path = resolve(path);
   fs.readdirSync(path).forEach(function(filename){
     var joined = join(path, filename);
-    if(fs.statSync(joined).isDirectory()){
-      retval[filename] = requireDirectory(m, joined);
-    }else{
-      var name = filename.substring(0, filename.lastIndexOf('.'));
-      retval[name] = m.require(joined);
+    if(delegate(joined)){
+      if(fs.statSync(joined).isDirectory()){
+        retval[filename] = requireDirectory(m, joined, delegate);
+      }else{
+        var name = filename.substring(0, filename.lastIndexOf('.'));
+        retval[name] = m.require(joined);
+      }
     }
   });
   return retval;
--- test/test.js
+++ test/test.js
@@ -4,11 +4,32 @@ var reqdir = require('../index');
 suite('require-directory', function(){
   suite('#', function(){
     test('should work', function(){
-      //arrange/act
+      //arrange
+      //act
       var test = reqdir(module, './test/example'); //path is slight weird because of mocha's module scoping
 
       //assert
       assert.equal('foo!', test.foo);
+      assert.equal('foo2!', test.foo2);
+      assert.equal('baz!', test.bar.baz);
+    });
+
+    test('should take an optional delegate', function(){
+      //arrange
+      var delegate = function(path){
+        if(/foo2.js$/.test(path)){
+          return false;
+        }else{
+          return true;
+        }
+      };
+
+      //act
+      var test = reqdir(module, './test/example', delegate); //path is slight weird because of mocha's module scoping
+
+      //assert
+      assert.equal('foo!', test.foo);
+      assert.equal(undefined, test.foo2);
       assert.equal('baz!', test.bar.baz);
     });
   });
