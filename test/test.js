9b4a5548481cacda0fbf9147893c082710630746
Troy Goode <troygoode@gmail.com>
2012-12-12 10:51:30 -0500

    refactored mocha test

--- test/test.js
+++ test/test.js
@@ -1,11 +1,13 @@
 var assert = require('assert');
+var reqdir = require('../index');
 
 suite('require-directory', function(){
   suite('#', function(){
     test('should work', function(){
-      var reqdir = require('./index');
-      var test = reqdir(module, './test/');
-      console.log(test);
+      //arrange/act
+      var test = reqdir(module, './test/example'); //path is slight weird because of mocha's module scoping
+
+      //assert
       assert.equal('foo!', test.foo);
       assert.equal('baz!', test.bar.baz);
     });
