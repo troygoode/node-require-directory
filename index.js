4823f2cf4e4fa7aacc2673155970c450968d0963
Troy Goode <troygoode@gmail.com>
2014-07-28 17:36:00 -0700

    added jshint

--- index.js
+++ index.js
@@ -1,55 +1,63 @@
-var fs = require('fs');
-var join = require('path').join;
-var resolve = require('path').resolve;
-var dirname = require('path').dirname;
+(function () {
+  'use strict';
 
-var requireDirectory = module.exports = function(m, path, exclude, callback){
-  var defaultDelegate = function(path, filename){
-    return filename[0] !== '.' && /\.(js|json|coffee)$/i.test(filename);
-  };
-  var delegate = defaultDelegate;
-  var retval = {};
+  var fs = require('fs'),
+    join = require('path').join,
+    resolve = require('path').resolve,
+    dirname = require('path').dirname;
 
-  // if no path was passed in, assume the equivelant of __dirname from caller
-  if(!path){
-    path = dirname(m.filename);
-  }
+  function requireDirectory(m, path, exclude, callback){
+    var defaultDelegate = function(path, filename){
+        return filename[0] !== '.' && /\.(js|json|coffee)$/i.test(filename);
+      },
+      delegate = defaultDelegate,
+      retval = {};
 
-  // if a RegExp was passed in as exclude, create a delegate that blacklists that RegExp
-  // if a function was passed in as exclude, use that function as the delegate
-  // default to an always-yes delegate
-  if(exclude instanceof RegExp){
-    delegate = function(path, filename){
-      if(!defaultDelegate(path, filename)){
-        return false;
-      }else if(exclude.test(path)){
-        return false;
+    // if no path was passed in, assume the equivelant of __dirname from caller
+    if(!path){
+      path = dirname(m.filename);
+    }
+
+    // if a RegExp was passed in as exclude, create a delegate that blacklists that RegExp
+    // if a function was passed in as exclude, use that function as the delegate
+    // default to an always-yes delegate
+    if(exclude instanceof RegExp){
+      delegate = function(path, filename){
+        if(!defaultDelegate(path, filename)){
+          return false;
+        }else if(exclude.test(path)){
+          return false;
+        }else{
+          return true;
+        }
+      };
+    }else if(exclude && {}.toString.call(exclude) === '[object Function]'){
+      delegate = exclude;
+    }
+
+    // get the path of each file in specified directory, append to current tree node, recurse
+    path = resolve(path);
+    fs.readdirSync(path).forEach(function(filename){
+      var joined = join(path, filename);
+      if(fs.statSync(joined).isDirectory()){
+        var files = requireDirectory(m, joined, delegate, callback); // this node is a directory; recurse
+        if (Object.keys(files).length){
+          retval[filename] = files;
+        }
       }else{
-        return true;
-      }
-    };
-  }else if(exclude && {}.toString.call(exclude) === '[object Function]'){
-    delegate = exclude;
-  }
-
-  // get the path of each file in specified directory, append to current tree node, recurse
-  path = resolve(path);
-  fs.readdirSync(path).forEach(function(filename){
-    var joined = join(path, filename);
-    if(fs.statSync(joined).isDirectory()){
-      var files = requireDirectory(m, joined, delegate, callback); // this node is a directory; recurse
-      if (Object.keys(files).length){
-        retval[filename] = files;
-      }
-    }else{
-      if(joined !== m.filename && delegate(joined, filename)){
-        var name = filename.substring(0, filename.lastIndexOf('.')); // hash node shouldn't include file extension
-        retval[name] = m.require(joined);
-        if (callback && typeof(callback) === 'function') {
-          callback(null, retval[name]);
+        if(joined !== m.filename && delegate(joined, filename)){
+          var name = filename.substring(0, filename.lastIndexOf('.')); // hash node shouldn't include file extension
+          retval[name] = m.require(joined);
+          if (callback && typeof(callback) === 'function') {
+            callback(null, retval[name]);
+          }
         }
       }
-    }
-  });
-  return retval;
-};
+    });
+    return retval;
+  }
+
+  module.exports = requireDirectory;
+
+}());
+
--- test/test.js
+++ test/test.js
@@ -1,95 +1,101 @@
-var assert = require('assert');
-var reqdir = require('../index');
-var PATH_TO_EXAMPLE = './test/example'; //path is slight weird because of mocha's module scoping
+/* global suite:true, test:true */
+(function () {
+  'use strict';
 
-suite('require-directory', function(){
-  test('should work', function(){
-    //arrange
-    //act
-    var test = reqdir(module, PATH_TO_EXAMPLE);
+  var assert = require('assert'),
+    reqdir = require('../index'),
+    PATH_TO_EXAMPLE = './test/example'; //path is slight weird because of mocha's module scoping
 
-    //assert
-    assert.equal('foo!', test.foo);
-    assert.equal('foo2!', test.foo2);
-    assert.equal('baz!', test.bar.baz);
-    assert.deepEqual({should:'be'}, test.bun);
-  });
-
-  test('should take an optional delegate function', function(){
-    //arrange
-    var delegate = function(path, filename){
-      if(/foo2.js$/.test(path) || filename[0] === '.'){
-        return false;
-      }else{
-        return true;
-      }
-    };
-
-    //act
-    var test = reqdir(module, PATH_TO_EXAMPLE, delegate);
-
-    //assert
-    assert.equal('foo!', test.foo);
-    assert.equal(undefined, test.foo2);
-    assert.equal('baz!', test.bar.baz);
-  });
-
-  test('should take an optional regex blacklist definition', function(){
-    //arrange
-    var blacklist = /foo2.js$/;
-
-    //act
-    var test = reqdir(module, PATH_TO_EXAMPLE, blacklist);
-
-    //assert
-    assert.equal('foo!', test.foo);
-    assert.equal(undefined, test.foo2);
-    assert.equal('baz!', test.bar.baz);
-  });
-
-  test('should exclude an empty directory', function(){
-    //act
-    var test = reqdir(module, PATH_TO_EXAMPLE);
-
-    //assert
-    assert.equal('foo!', test.foo);
-    assert.equal(undefined, test.empty);
-  });
-
-  test('should exclude directory only matching blacklist', function(){
-    //arrange
-    var blacklist = /baz.js$/;
-
-    //act
-    var test = reqdir(module, PATH_TO_EXAMPLE, blacklist);
-
-    //assert
-    assert.equal('foo!', test.foo);
-    assert.equal('foo2!', test.foo2);
-    assert.equal(undefined, test.bar);
-  });
-
-  test('index should exclude itself', function(){
-    //act
-    var index = require('./example/index');
-
-    //assert
-    assert.equal('foo!', index.foo);
-    assert.equal(undefined, index.index);
-  });
-
-  test('should take an optional callback', function(done) {
+  suite('require-directory', function(){
+    test('should work', function(){
       //arrange
-      var callback = function(err, mod) {
-              var result = mod();
-              //assert
-              assert.equal('gone and done it', result);
-              done();
+      //act
+      var test = reqdir(module, PATH_TO_EXAMPLE);
+
+      //assert
+      assert.equal('foo!', test.foo);
+      assert.equal('foo2!', test.foo2);
+      assert.equal('baz!', test.bar.baz);
+      assert.deepEqual({should:'be'}, test.bun);
+    });
+
+    test('should take an optional delegate function', function(){
+      //arrange
+      var delegate = function(path, filename){
+        if(/foo2.js$/.test(path) || filename[0] === '.'){
+          return false;
+        }else{
+          return true;
+        }
       };
-      var path = PATH_TO_EXAMPLE + '/fun';
 
       //act
-      reqdir(module, path, null, callback);
+      var test = reqdir(module, PATH_TO_EXAMPLE, delegate);
+
+      //assert
+      assert.equal('foo!', test.foo);
+      assert.equal(undefined, test.foo2);
+      assert.equal('baz!', test.bar.baz);
+    });
+
+    test('should take an optional regex blacklist definition', function(){
+      //arrange
+      var blacklist = /foo2.js$/;
+
+      //act
+      var test = reqdir(module, PATH_TO_EXAMPLE, blacklist);
+
+      //assert
+      assert.equal('foo!', test.foo);
+      assert.equal(undefined, test.foo2);
+      assert.equal('baz!', test.bar.baz);
+    });
+
+    test('should exclude an empty directory', function(){
+      //act
+      var test = reqdir(module, PATH_TO_EXAMPLE);
+
+      //assert
+      assert.equal('foo!', test.foo);
+      assert.equal(undefined, test.empty);
+    });
+
+    test('should exclude directory only matching blacklist', function(){
+      //arrange
+      var blacklist = /baz.js$/;
+
+      //act
+      var test = reqdir(module, PATH_TO_EXAMPLE, blacklist);
+
+      //assert
+      assert.equal('foo!', test.foo);
+      assert.equal('foo2!', test.foo2);
+      assert.equal(undefined, test.bar);
+    });
+
+    test('index should exclude itself', function(){
+      //act
+      var index = require('./example/index');
+
+      //assert
+      assert.equal('foo!', index.foo);
+      assert.equal(undefined, index.index);
+    });
+
+    test('should take an optional callback', function(done) {
+        //arrange
+        var callback = function(err, mod) {
+                var result = mod();
+                //assert
+                assert.equal('gone and done it', result);
+                done();
+        };
+        var path = PATH_TO_EXAMPLE + '/fun';
+
+        //act
+        reqdir(module, path, null, callback);
+    });
   });
-});
+
+}());
 
