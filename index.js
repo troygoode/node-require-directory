adc3e2511bbcea89e2b5093f238d2253d5b5c2a5
Troy Goode <troygoode@gmail.com>
2014-07-28 17:57:48 -0700

    now using an options object instead of a ton of parameters. woooo

--- index.js
+++ index.js
@@ -6,12 +6,15 @@
     resolve = require('path').resolve,
     dirname = require('path').dirname;
 
-  function requireDirectory(m, path, exclude, callback){
+  function requireDirectory(m, path, options){
     var defaultDelegate = function(path, filename){
         return filename[0] !== '.' && /\.(js|json|coffee)$/i.test(filename);
       },
       delegate = defaultDelegate,
       retval = {};
+
+    // default options
+    options = options || {};
 
     // if no path was passed in, assume the equivelant of __dirname from caller
     if(!path){
@@ -21,18 +24,18 @@
     // if a RegExp was passed in as exclude, create a delegate that blacklists that RegExp
     // if a function was passed in as exclude, use that function as the delegate
     // default to an always-yes delegate
-    if(exclude instanceof RegExp){
+    if(options.include instanceof RegExp){
       delegate = function(path, filename){
         if(!defaultDelegate(path, filename)){
           return false;
-        }else if(exclude.test(path)){
-          return false;
-        }else{
+        }else if(options.include.test(path)){
           return true;
+        }else{
+          return false;
         }
       };
-    }else if(exclude && {}.toString.call(exclude) === '[object Function]'){
-      delegate = exclude;
+    }else if(options.include && {}.toString.call(options.include) === '[object Function]'){
+      delegate = options.include;
     }
 
     // get the path of each file in specified directory, append to current tree node, recurse
@@ -40,7 +43,7 @@
     fs.readdirSync(path).forEach(function(filename){
       var joined = join(path, filename);
       if(fs.statSync(joined).isDirectory()){
-        var files = requireDirectory(m, joined, delegate, callback); // this node is a directory; recurse
+        var files = requireDirectory(m, joined, options); // this node is a directory; recurse
         if (Object.keys(files).length){
           retval[filename] = files;
         }
@@ -48,8 +51,8 @@
         if(joined !== m.filename && delegate(joined, filename)){
           var name = filename.substring(0, filename.lastIndexOf('.')); // hash node shouldn't include file extension
           retval[name] = m.require(joined);
-          if (callback && typeof(callback) === 'function') {
-            callback(null, retval[name]);
+          if (options.visit && typeof(options.visit) === 'function') {
+            options.visit(null, retval[name]);
           }
         }
       }
--- test/test.js
+++ test/test.js
@@ -6,8 +6,8 @@
     reqdir = require('../index'),
     PATH_TO_EXAMPLE = './test/example'; //path is slight weird because of mocha's module scoping
 
-  suite('require-directory', function(){
-    test('should work', function(){
+  suite('require-directory', function () {
+    test('should work with JS', function () {
       //arrange
       //act
       var test = reqdir(module, PATH_TO_EXAMPLE);
@@ -15,22 +15,38 @@
       //assert
       assert.equal('foo!', test.foo);
       assert.equal('foo2!', test.foo2);
-      assert.equal('baz!', test.bar.baz);
-      assert.deepEqual({should:'be'}, test.bun);
     });
 
-    test('should take an optional delegate function', function(){
+    test('should work with JSON', function () {
+      //act
+      var test = reqdir(module, PATH_TO_EXAMPLE);
+
+      //assert
+      assert.equal('be', test.bun.should);
+    });
+
+    test('should work with nested folders', function () {
       //arrange
-      var delegate = function(path, filename){
-        if(/foo2.js$/.test(path) || filename[0] === '.'){
+      //act
+      var test = reqdir(module, PATH_TO_EXAMPLE);
+
+      //assert
+      assert.equal('foo!', test.foo);
+      assert.equal('baz!', test.bar.baz);
+    });
+
+    test('should take an optional delegate function', function () {
+      //arrange
+      var delegate = function (path, filename) {
+        if (/foo2.js$/.test(path) || filename[0] === '.') {
           return false;
-        }else{
+        } else {
           return true;
         }
       };
 
       //act
-      var test = reqdir(module, PATH_TO_EXAMPLE, delegate);
+      var test = reqdir(module, PATH_TO_EXAMPLE, {include: delegate});
 
       //assert
       assert.equal('foo!', test.foo);
@@ -38,20 +54,20 @@
       assert.equal('baz!', test.bar.baz);
     });
 
-    test('should take an optional regex blacklist definition', function(){
+    test('should take an optional regex whitelist definition', function () {
       //arrange
-      var blacklist = /foo2.js$/;
+      var whitelist = /(foo|foo2).js$/;
 
       //act
-      var test = reqdir(module, PATH_TO_EXAMPLE, blacklist);
+      var test = reqdir(module, PATH_TO_EXAMPLE, {include: whitelist});
 
       //assert
       assert.equal('foo!', test.foo);
-      assert.equal(undefined, test.foo2);
-      assert.equal('baz!', test.bar.baz);
+      assert.equal('foo2!', test.foo2);
+      assert.equal(undefined, test.bun);
     });
 
-    test('should exclude an empty directory', function(){
+    test('should exclude an empty directory', function () {
       //act
       var test = reqdir(module, PATH_TO_EXAMPLE);
 
@@ -60,20 +76,19 @@
       assert.equal(undefined, test.empty);
     });
 
-    test('should exclude directory only matching blacklist', function(){
+    test('should exclude directory with no files on matching whitelist', function () {
       //arrange
-      var blacklist = /baz.js$/;
+      var whitelist = /foo.js$/;
 
       //act
-      var test = reqdir(module, PATH_TO_EXAMPLE, blacklist);
+      var test = reqdir(module, PATH_TO_EXAMPLE, {include: whitelist});
 
       //assert
       assert.equal('foo!', test.foo);
-      assert.equal('foo2!', test.foo2);
       assert.equal(undefined, test.bar);
     });
 
-    test('index should exclude itself', function(){
+    test('index should exclude itself', function () {
       //act
       var index = require('./example/index');
 
@@ -82,18 +97,18 @@
       assert.equal(undefined, index.index);
     });
 
-    test('should take an optional callback', function(done) {
-        //arrange
-        var callback = function(err, mod) {
-                var result = mod();
-                //assert
-                assert.equal('gone and done it', result);
-                done();
-        };
-        var path = PATH_TO_EXAMPLE + '/fun';
+    test('should take an optional callback', function (done) {
+      //arrange
+      var callback = function (err, mod) {
+          var result = mod();
+          //assert
+          assert.equal('gone and done it', result);
+          done();
+        },
+        path = PATH_TO_EXAMPLE + '/fun';
 
-        //act
-        reqdir(module, path, null, callback);
+      //act
+      reqdir(module, path, {visit: callback});
     });
   });
 
