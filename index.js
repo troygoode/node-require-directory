3733212c42fa8d15848d53bdf57a8604052141c3
Noor Alhiraki <noor@avaaz.org>
2015-02-14 13:10:16 +0200

    Refactoring and code styling.

--- index.js
+++ index.js
@@ -1,101 +1,86 @@
-(function () {
-  'use strict';
+'use strict';
 
-  var fs = require('fs'),
-    join = require('path').join,
-    resolve = require('path').resolve,
-    dirname = require('path').dirname,
-    defaultOptions = {
-      extensions: ['js', 'json', 'coffee'],
-      recurse: true,
-      rename: function (name) {
-        return name;
-      },
-      visit: function (obj) {
-        return obj;
-      }
-    };
-
-  function requireDirectory(m, path, options) {
-    var retval = {},
-      includeFile = null;
-
-    // path is optional
-    if (path && !options && typeof path !== 'string') {
-      options = path;
-      path = null;
+var fs = require('fs'),
+  join = require('path').join,
+  resolve = require('path').resolve,
+  dirname = require('path').dirname,
+  defaultOptions = {
+    extensions: ['js', 'json', 'coffee'],
+    recurse: true,
+    rename: function (name) {
+      return name;
+    },
+    visit: function (obj) {
+      return obj;
     }
+  };
 
-    // default options
-    options = options || {};
-    for (var prop in defaultOptions) {
-      if (typeof options[prop] === 'undefined') {
-        options[prop] = defaultOptions[prop];
-      }
-    }
+function checkFileInclusion(path, filename, options) {
+  return (
+    // verify file has valid extension
+    (new RegExp('\\.(' + options.extensions.join('|') + ')$', 'i').test(filename)) &&
 
-    // if no path was passed in, assume the equivelant of __dirname from caller
-    // otherwise, resolve path relative to the equivalent of __dirname
-    if (!path) {
-      path = dirname(m.filename);
-    } else {
-      path = resolve(dirname(m.filename), path);
-    }
+    // if options.include is a RegExp, evaluate it and make sure the path passes
+    !(options.include && options.include instanceof RegExp && !options.include.test(path)) &&
 
-    includeFile = function (path, filename) {
-      // verify file has valid extension
-      if (!new RegExp('\\.(' + options.extensions.join('|') + ')$', 'i').test(filename)) {
-        return false;
-      }
+    // if options.include is a function, evaluate it and make sure the path passes
+    !(options.include && typeof options.include === 'function' && !options.include(path, filename)) &&
 
-      // if options.include is a RegExp, evaluate it and make sure the path passes
-      if (options.include && options.include instanceof RegExp && !options.include.test(path)) {
-        return false;
-      }
+    // if options.exclude is a RegExp, evaluate it and make sure the path doesn't pass
+    !(options.exclude && options.exclude instanceof RegExp && options.exclude.test(path)) &&
 
-      // if options.include is a function, evaluate it and make sure the path passes
-      if (options.include && typeof options.include === 'function' && !options.include(path, filename)) {
-        return false;
-      }
+    // if options.exclude is a function, evaluate it and make sure the path doesn't pass
+    !(options.exclude && typeof options.exclude === 'function' && options.exclude(path, filename))
+  );
+}
 
-      // if options.exclude is a RegExp, evaluate it and make sure the path doesn't pass
-      if (options.exclude && options.exclude instanceof RegExp && options.exclude.test(path)) {
-        return false;
-      }
+function requireDirectory(m, path, options) {
+  var retval = {};
 
-      // if options.exclude is a function, evaluate it and make sure the path doesn't pass
-      if (options.exclude && typeof options.exclude === 'function' && options.exclude(path, filename)) {
-        return false;
-      }
-
-      return true;
-    };
-
-    // get the path of each file in specified directory, append to current tree node, recurse
-    path = resolve(path);
-    fs.readdirSync(path).forEach(function (filename) {
-      var joined = join(path, filename),
-        files,
-        key,
-        obj;
-      if (fs.statSync(joined).isDirectory() && options.recurse) {
-        files = requireDirectory(m, joined, options); // this node is a directory; recurse
-        if (Object.keys(files).length) { // exclude empty directories
-          retval[options.rename(filename, joined, filename)] = files;
-        }
-      } else {
-        if (joined !== m.filename && includeFile(joined, filename)) {
-          key = filename.substring(0, filename.lastIndexOf('.')); // hash node key shouldn't include file extension
-          obj = m.require(joined);
-          retval[options.rename(key, joined, filename)] = options.visit(obj, joined, filename) || obj;
-        }
-      }
-    });
-    return retval;
+  // path is optional
+  if (path && !options && typeof path !== 'string') {
+    options = path;
+    path = null;
   }
 
-  module.exports = requireDirectory;
-  module.exports.defaults = defaultOptions;
+  // default options
+  options = options || {};
+  for (var prop in defaultOptions) {
+    if (typeof options[prop] === 'undefined') {
+      options[prop] = defaultOptions[prop];
+    }
+  }
 
-}());
+  // if no path was passed in, assume the equivelant of __dirname from caller
+  // otherwise, resolve path relative to the equivalent of __dirname
+  path = !path ? dirname(m.filename) : resolve(dirname(m.filename), path);
 
+  // get the path of each file in specified directory, append to current tree node, recurse
+  fs.readdirSync(path).forEach(function (filename) {
+    var joined = join(path, filename),
+      files,
+      key,
+      obj;
+
+    if (fs.statSync(joined).isDirectory() && options.recurse) {
+      // this node is a directory; recurse
+      files = requireDirectory(m, joined, options);
+      // exclude empty directories
+      if (Object.keys(files).length) {
+        retval[options.rename(filename, joined, filename)] = files;
+      }
+    } else {
+      if (joined !== m.filename && checkFileInclusion(joined, filename, options)) {
+        // hash node key shouldn't include file extension
+        key = filename.substring(0, filename.lastIndexOf('.'));
+        obj = m.require(joined);
+        retval[options.rename(key, joined, filename)] = options.visit(obj, joined, filename) || obj;
+      }
+    }
+  });
+
+  return retval;
+}
+
+module.exports = requireDirectory;
+module.exports.defaults = defaultOptions;
